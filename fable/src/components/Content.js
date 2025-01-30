43643e423229e2d150dbeac63ee48188ad011c00
Abigail Trebon <ab910876@ucf.edu>
2025-01-29 19:13:10 -0500

    commit message

---
+++ fable/src/components/Content.js
@@ -0,0 +1,10 @@
+function Content() {
+    return (
+        <div>
+            <p>The Ant and The Grasshopper is story about a hardworking ant and a carefree grasshopper. The ant in the story spends her summer working hard to gather food and prepare for the winter, while the grasshopper spends his time playing around. The grasshopper thought it was silly that the ant was working so hard, while the ant thought the grasshopper was silly for wasting his time singing and playing.</p>
+            <p>Finally, winter comes. The ant is prepared! She has food, shelter, and warmth to keep her safe. The grasshopper has nothing. He goes to the ant pleading for help. The ant declines and the grasshopper is left cold. He realizes that he should have prepared in the warm months like the ant. </p>
+        </div>
+    );
+};
+
+export default Content;
---
+++ fable/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div>
+            <h2>The moral of the story is to use your time wisely, or work hard now so you can play later. </h2>
+        </div>
+    );
+};
+
+export default Footer;
